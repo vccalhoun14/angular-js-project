303e4617b78525c7981d94dae1d4c4a2e60e111e
Your Name <vccalhoun08@aol.com>
2014-10-22 13:54:49 -0700

    Removing build/ dir and including in .gitignore. Also pushing some updates from a while ago

--- victoria-jenny-matt/Gruntfile.js
+++ victoria-jenny-matt/Gruntfile.js
@@ -6,8 +6,28 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-css');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
 
   grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/style.css': 'sass/style.scss'
+        }
+      }
+    },
+
+    watch: {
+      source: {
+        files: ['sass/**/*.scss', 'views/**/*.jade', 'app/css/**/*.css'],
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+
     clean: {
       dev: {
         src: ['build/']
@@ -36,5 +56,5 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev'])
+  grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev', 'watch'])
 }
